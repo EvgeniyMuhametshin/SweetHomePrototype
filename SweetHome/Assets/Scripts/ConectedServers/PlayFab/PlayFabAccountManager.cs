66b3da6eaa8f3b08c9107c29c9c854faffc05d00
Evgeniy Muhamethin <John090918@gmail.com>
2022-07-29 10:59:52 +0300

    Lesson 8

--- SweetHome/Assets/Scripts/ConectedServers/PlayFab/PlayFabAccountManager.cs
+++ SweetHome/Assets/Scripts/ConectedServers/PlayFab/PlayFabAccountManager.cs
@@ -1,13 +1,29 @@
 using PlayFab;
 using PlayFab.ClientModels;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayFabAccountManager : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text _titleLabel;
+
+	[SerializeField]
+	private GameObject _newCharacterCreatePanel;
+
+	[SerializeField]
+	private Button _creatCharacterButton;
+
+	[SerializeField]
+	private TMP_InputField _inputField;
+
+	[SerializeField]
+	private List<SlotCharacterWidged> _slots;
+
+	private string _characterName;
 
 	private void Start()
 	{
@@ -15,6 +31,100 @@ public class PlayFabAccountManager : MonoBehaviour
 			OnGetAccount, OnError);
 		PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(),
 			OnGetCatalog, OnError);
+
+		GetChacters();
+
+		foreach (var slot in _slots)
+		{
+			slot.Button.onClick.AddListener(OpenCreateNewCharacter);
+		}
+
+		_creatCharacterButton.onClick.AddListener(CreateCharacterWithItem);
+
+		_inputField.onValueChanged.AddListener(OnNameChanged);
+	}
+
+	private void CreateCharacterWithItem()
+	{
+		PlayFabClientAPI.GrantCharacterToUser(new GrantCharacterToUserRequest()
+		{
+			CharacterName = _characterName,
+			ItemId = "character_item"
+		}, result => 
+		{
+			UpdateCharacterStatisticsRequest(result.CharacterId);
+		}, OnError);
+	}
+
+	private void UpdateCharacterStatisticsRequest(string characterId)
+	{
+		PlayFabClientAPI.UpdateCharacterStatistics(new UpdateCharacterStatisticsRequest()
+		{
+			CharacterId = characterId,
+			CharacterStatistics = new Dictionary<string, int>()
+			{
+				{ "Level", 1 },
+				{ "Gold", 0}
+			}
+		}, result =>
+		{
+			Debug.Log("UpdateCharacterStatistics complete!!!");
+			CloseCreateNewCharacter();
+			GetChacters();
+		}, OnError);
+	}
+
+	private void GetChacters()
+	{
+		PlayFabClientAPI.GetAllUsersCharacters(new ListUsersCharactersRequest(),
+			result =>
+			{
+				Debug.Log($"Character count: {result.Characters.Count}");
+				ShowCharacterInSlot(result.Characters);
+			}, OnError);
+	}
+
+	private void ShowCharacterInSlot(List<CharacterResult> characters)
+	{
+		if (characters.Count == 0)
+		{
+			foreach (var slot in _slots)
+			{
+				slot.ShowEmptySlot();
+			}
+		}
+		else if (characters.Count > 0 && characters.Count <= _slots.Count)
+		{
+			PlayFabClientAPI.GetCharacterStatistics(new GetCharacterStatisticsRequest()
+			{
+				CharacterId = characters.First().CharacterId
+			}, result =>
+			{
+				var level = result.CharacterStatistics["Level"].ToString();
+				var gold = result.CharacterStatistics["Gold"].ToString();
+
+				_slots.First().ShowInfoCharacterSlot(characters.First().CharacterName, level, gold);
+			}, OnError);
+		}
+		else
+		{
+			Debug.LogError("Added slots of characters");
+		}
+	}
+
+	private void OpenCreateNewCharacter()
+	{
+		_newCharacterCreatePanel.SetActive(true);
+	}
+	
+	private void CloseCreateNewCharacter()
+	{
+		_newCharacterCreatePanel.SetActive(false);
+	}
+
+	private void OnNameChanged(string changedName)
+	{
+		_characterName = changedName;
 	}
 
 	private void OnGetCatalog(GetCatalogItemsResult result)
